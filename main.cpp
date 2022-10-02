e6885783b56358f88ae1a719537e1708887b881f
deppenkaiser <contact@log1cworx.at>
2022-10-02 10:38:42 +0200

    GIT shit

--- main.cpp
+++ main.cpp
@@ -11,30 +11,7 @@ int main()
     actions.push_back({Rule({Expression({Token::NA})}), Expression({Token::B})});
     actions.push_back({Rule({Expression({Token::B})}), Expression({Token::A, Token::NB})});
 
-    bool bLoopAgain = false;
-    do
-    {
-        bLoopAgain = false;
-        for (const Action& action : actions)
-        {
-            if (isRuleInFacts(facts, action.rule))
-            {
-                for (const Token& token : action.output)
-                {
-                    if (token % 2)
-                    {
-                        addToken(&facts, token);
-                    }
-                    else
-                    {
-                        removeToken(&facts, token);
-                    }
-                }
-
-                bLoopAgain = true;
-            }
-        }
-    } while(bLoopAgain);
+    while(executeProgram(actions, &facts));
 
     return 0;
 }
