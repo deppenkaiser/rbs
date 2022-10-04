811f587c775b3111df01e3b5a769a20d7456d3b5
deppenkaiser <contact@log1cworx.at>
2022-10-04 20:09:40 +0200

    ADD refactored

--- SM.h
+++ SM.h
@@ -13,7 +13,41 @@ enum SweenBob
 
 class SM : public RBS<SweenBob>
 {
+	private:
+	    Actions m_actions;
+
 	public:
+		SM()
+		{
+			Expression exp_in_00({SweenBob::N_INITIALIZE, SweenBob::N_STOP});
+			Rule rule_00({exp_in_00});
+			Expression exp_out_00({SweenBob::INITIALIZE, SweenBob::CHECK_STOP});
+			Action act_00({rule_00, exp_out_00});
+
+			Expression exp_in_01({SweenBob::INITIALIZE});
+			Rule rule_01({exp_in_01});
+			Expression exp_out_01({SweenBob::CALCULATE_COUNTER});
+			Action act_01({rule_01, exp_out_01});
+
+			Expression exp_in_02({SweenBob::CHECK_STOP});
+			Rule rule_02({exp_in_02});
+			Expression exp_out_02({SweenBob::STOP, SweenBob::N_INITIALIZE, SweenBob::N_CALCULATE_COUNTER, SweenBob::N_CHECK_STOP});
+			Action act_02({rule_02, exp_out_02});
+
+			m_actions.push_back(act_00);
+			m_actions.push_back(act_01);
+			m_actions.push_back(act_02);
+		}
+
+		virtual ~SM()
+		{
+		}
+
+		bool executeProgram()
+		{
+			return RBS<SweenBob>::executeProgram(m_actions);
+		}
+
 		virtual bool calculateInputValue(const SweenBob& token)
 		{
 			bool bRetVal = true;
--- main.cpp
+++ main.cpp
@@ -3,17 +3,12 @@
 
 int main()
 {
-    SM::Actions actions;
     SM sm;
-
-    actions.push_back({SM::Rule({SM::Expression({SweenBob::N_INITIALIZE, SweenBob::N_STOP})}), SM::Expression({SweenBob::INITIALIZE, SweenBob::CHECK_STOP})});
-    actions.push_back({SM::Rule({SM::Expression({SweenBob::INITIALIZE})}), SM::Expression({SweenBob::CALCULATE_COUNTER})});
-    actions.push_back({SM::Rule({SM::Expression({SweenBob::CHECK_STOP})}), SM::Expression({SweenBob::STOP, SweenBob::N_INITIALIZE, SweenBob::N_CALCULATE_COUNTER, SweenBob::N_CHECK_STOP})});
 
     bool bLoop = false;
     do
     {
-        bLoop = sm.executeProgram(actions);
+        bLoop = sm.executeProgram();
     } while (bLoop);
 
     return 0;
