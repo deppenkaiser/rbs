6f9ab4d39dd9f3bee89e45a7ce6409bd8e25f59a
Dipl.-Ing. (FH) Michael Czybor <Michael.Czybor@Live.de>
2026-08-24 15:21:01 +0200

    ...

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <memory.h>
 
 typedef enum token
 {
@@ -11,9 +12,20 @@ typedef enum token
 	TOKEN_COUNT
 }* token_t;
 
+typedef enum value
+{
+	AGE,
+	MONEY,
+	SPEED,
+	VALUE_COUNT
+}* value_t;
+
+typedef double* memory_t;
+
 typedef struct rbs
 {
 	token_t facts;
+	memory_t memory;
 }* rbs_t;
 
 bool rbs_check_token_is_true(enum token token)
@@ -44,9 +56,19 @@ uint32_t rbs_calculate_token_count()
 	return TOKEN_COUNT * 2 - 1;
 }
 
+uint32_t rbs_calculate_value_count()
+{
+	return VALUE_COUNT;
+}
+
 token_t rbs_create_facts_buffer()
 {
 	return malloc(rbs_calculate_token_count() * sizeof(enum token));
+}
+
+memory_t rbs_create_memory_buffer()
+{
+	return malloc(rbs_calculate_value_count() * sizeof(double));
 }
 
 void rbs_destroy_facts_buffer(token_t* ptoken)
@@ -58,6 +80,15 @@ void rbs_destroy_facts_buffer(token_t* ptoken)
 	}
 }
 
+void rbs_destroy_memory_buffer(memory_t* pmemory)
+{
+	if (*pmemory != NULL)
+	{
+		free(*pmemory);
+		*pmemory = NULL;
+	}
+}
+
 void rbs_initialize_facts(token_t facts)
 {
 	for (size_t i = 1; i < TOKEN_COUNT; ++i)
@@ -66,16 +97,24 @@ void rbs_initialize_facts(token_t facts)
 	}
 }
 
+void rbs_initialize_memory(memory_t memory)
+{
+	memset(memory, 0, rbs_calculate_value_count() * sizeof(double));
+}
+
 int main()
 {
 	struct rbs rbs = 
 	{
-		.facts = rbs_create_facts_buffer()
+		.facts = rbs_create_facts_buffer(),
+		.memory = rbs_create_memory_buffer()
 	};
 
 	rbs_initialize_facts(rbs.facts);
+	rbs_initialize_memory(rbs.memory);
 
 	rbs_destroy_facts_buffer(&rbs.facts);
+	rbs_destroy_memory_buffer(&rbs.memory);
 
 	return 0;
 }
