8208f8824754be27f38226831e9c6d107424d013
Dipl.-Ing. (FH) Michael Czybor <Michael.Czybor@Live.de>
2026-08-28 00:26:02 +0200

    ...

--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@
 
 typedef enum token
 {
+	N_ADULT = -6,
 	N_UMBRELLA = -5,
 	N_WET = -4,
 	N_CLOUDY = -3,
@@ -18,6 +19,7 @@ typedef enum token
 	CLOUDY = 3,
 	WET = 4,
 	UMBRELLA = 5,
+	ADULT = 6,
 	TOKEN_COUNT
 }* token_t;
 
@@ -29,7 +31,32 @@ typedef enum value
 	VALUE_COUNT
 }* value_t;
 
+typedef enum operation
+{
+	NONE = 0,
+	ADD = 1,
+	SUB = 2,
+	MUL = 3,
+	DIV = 4,
+	EQ = 5,
+	NE = 6,
+	LT = 7,
+	LE = 8,
+	GT = 9,
+	GE = 10,
+	OPERATION_COUNT
+}* operation_t;
+
 typedef double* memory_t;
+
+typedef struct rbs_term
+{
+	bool comparison;
+	enum token fact;
+	enum value value;
+	enum operation op;
+	double operand;
+} rbs_term_t;
 
 typedef struct rbs
 {
@@ -39,7 +66,7 @@ typedef struct rbs
 
 typedef struct rbs_rule
 {
-	const enum token* if_facts;
+	const rbs_term_t* if_terms;
 	const enum token* then_facts;
 } rbs_rule_t;
 
@@ -133,6 +160,32 @@ void rbs_initialize_memory(memory_t memory)
 	memset(memory, 0, rbs_calculate_value_count() * sizeof(double));
 }
 
+bool rbs_compare(memory_t memory, enum value value, enum operation op, double operand)
+{
+	double lhs = memory[(size_t) value];
+	int32_t op_value = op;
+
+	switch (op_value)
+	{
+		case EQ: return lhs == operand;
+		case NE: return lhs != operand;
+		case LT: return lhs < operand;
+		case LE: return lhs <= operand;
+		case GT: return lhs > operand;
+		case GE: return lhs >= operand;
+		default: return false;
+	}
+}
+
+bool rbs_term_is_true(struct rbs* rbs, rbs_term_t term)
+{
+	if (term.comparison)
+	{
+		return rbs_compare(rbs->memory, term.value, term.op, term.operand);
+	}
+	return rbs_is_fact(rbs->facts, term.fact);
+}
+
 void rbs_fire(struct rbs* rbs, const rbs_rule_t* rules, size_t rule_count)
 {
 	for (size_t i = 0; i < rule_count; ++i)
@@ -140,9 +193,9 @@ void rbs_fire(struct rbs* rbs, const rbs_rule_t* rules, size_t rule_count)
 		const rbs_rule_t* rule = &rules[i];
 		bool matches = true;
 
-		for (const enum token* fact = rule->if_facts; *fact != ZERO; ++fact)
+		for (const rbs_term_t* term = rule->if_terms; !(term->fact == ZERO && !term->comparison); ++term)
 		{
-			if (!rbs_is_fact(rbs->facts, *fact))
+			if (!rbs_term_is_true(rbs, *term))
 			{
 				matches = false;
 				break;
@@ -159,15 +212,31 @@ void rbs_fire(struct rbs* rbs, const rbs_rule_t* rules, size_t rule_count)
 	}
 }
 
-const enum token if_weather[] = { RAIN, CLOUDY, ZERO };
+const rbs_term_t if_weather[] =
+{
+	{ .comparison = false, .fact = RAIN },
+	{ .comparison = false, .fact = CLOUDY },
+	{ .comparison = false, .fact = ZERO }
+};
 const enum token then_weather[] = { WET, ZERO };
-const enum token if_wet[] = { WET, ZERO };
+const rbs_term_t if_wet[] =
+{
+	{ .comparison = false, .fact = WET },
+	{ .comparison = false, .fact = ZERO }
+};
 const enum token then_wet[] = { UMBRELLA, ZERO };
+const rbs_term_t if_adult[] =
+{
+	{ .comparison = true, .value = AGE, .op = GT, .operand = 18 },
+	{ .comparison = false, .fact = ZERO }
+};
+const enum token then_adult[] = { ADULT, ZERO };
 
 const rbs_rule_t rules[] =
 {
 	{ if_weather, then_weather },
-	{ if_wet, then_wet }
+	{ if_wet, then_wet },
+	{ if_adult, then_adult }
 };
 
 int main()
@@ -181,6 +250,8 @@ int main()
 	rbs_initialize_facts(rbs.facts);
 	rbs_initialize_memory(rbs.memory);
 
+	rbs.memory[AGE] = 20;
+
 	rbs_set_fact(rbs.facts, RAIN);
 	rbs_set_fact(rbs.facts, CLOUDY);
 
@@ -190,6 +261,7 @@ int main()
 	printf("WET:      %s\n", rbs_is_fact(rbs.facts, WET) ? "true" : "false");
 	printf("UMBRELLA: %s\n", rbs_is_fact(rbs.facts, UMBRELLA) ? "true" : "false");
 	printf("NOT WET:  %s\n", rbs_is_fact(rbs.facts, N_WET) ? "true" : "false");
+	printf("ADULT:    %s\n", rbs_is_fact(rbs.facts, ADULT) ? "true" : "false");
 
 	rbs_destroy_facts_buffer(&rbs.facts);
 	rbs_destroy_memory_buffer(&rbs.memory);
